3e4f82f6fa3710a7b9fa6149b351a04572229b25
Danh0DIY <dnn345169@gmail.com>
2025-12-13 17:04:01 +0700

    Update home.php

--- home.php
+++ home.php
@@ -1,1 +1,19 @@
-
+<?php
+session_start();
+if (!isset($_SESSION['user'])) {
+    header("Location: index.html");
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+<meta charset="UTF-8">
+<title>Trang chủ</title>
+</head>
+<body>
+<h2>Xin chào <?php echo $_SESSION['user']; ?></h2>
+<p>Đăng nhập thành công</p>
+<a href="logout.php">Đăng xuất</a>
+</body>
+</html>
