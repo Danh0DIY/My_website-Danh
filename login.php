e2528c544b7557bdb672d118dbd3f4f23687ad28
Danh0DIY <dnn345169@gmail.com>
2025-12-13 17:02:33 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,1 +1,22 @@
+<?php
+session_start();
+require "connect.php";
+
+$username = mysqli_real_escape_string($conn, $_POST['username']);
+$password = $_POST['password'];
+
+$sql = "SELECT * FROM users WHERE username='$username'";
+$result = $conn->query($sql);
+
+if ($result->num_rows == 1) {
+    $row = $result->fetch_assoc();
+    if (password_verify($password, $row['password'])) {
+        $_SESSION['user'] = $username;
+        header("Location: home.php");
+        exit();
+    }
+}
+
+echo "Sai tài khoản hoặc mật khẩu!";
+?>
 
