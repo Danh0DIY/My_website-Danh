67d924a43378680d649a534c1104e699f4a2f9a7
Danh0DIY <dnn345169@gmail.com>
2025-12-13 17:03:26 +0700

    Implement user registration functionality

--- register.php
+++ register.php
@@ -1,1 +1,16 @@
+<?php
+require "connect.php";
+
+$username = mysqli_real_escape_string($conn, $_POST['username']);
+$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+$sql = "INSERT INTO users (username, password)
+        VALUES ('$username', '$password')";
+
+if ($conn->query($sql)) {
+    echo "Đăng ký thành công! <a href='index.html'>Đăng nhập</a>";
+} else {
+    echo "Lỗi: Tên đăng nhập đã tồn tại";
+}
+?>
 
