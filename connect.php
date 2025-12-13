cd4bc1d0b7cdde1abf8126ff7330ec14fa94dbee
Danh0DIY <dnn345169@gmail.com>
2025-12-13 15:42:18 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,1 +1,13 @@
+<?php
+$host = "RAILWAY_HOST";
+$user = "RAILWAY_USER";
+$pass = "RAILWAY_PASSWORD";
+$db   = "RAILWAY_DATABASE";
+$port = RAILWAY_PORT;
+
+$conn = new mysqli($host, $user, $pass, $db, $port);
+if ($conn->connect_error) {
+    die("Kết nối thất bại: " . $conn->connect_error);
+}
+?>
 
